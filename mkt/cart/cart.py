66496202a7f35f240539597acb7d5ede1b966f44
quincyocharles <quincyocharles@gmail.com>
2024-07-03 19:01:43 +0300

    Cart Totals set up in the cart summary page

--- mkt/cart/cart.py
+++ mkt/cart/cart.py
@@ -28,6 +28,23 @@ class Cart():
 
         self.session.modified = True
 
+    def cart_total(self):
+        # Get product IDS
+        product_ids = self.cart.keys()
+        # lookup keys in our db model
+        products = Product.objects.filter(id__in=product_ids)
+        # Get quantities
+        quantities = self.cart
+        # start counting at 0
+        total = 0
+        for key, value in quantities.items():
+            # Convert int string into int so we can do math
+            key = int(key)
+            for product in products:
+                if product.id == key:
+                    total = total + (product.price * value)
+        return total
+
     def __len__(self):
         return len(self.cart)
 
