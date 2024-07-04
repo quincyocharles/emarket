ce5c73d486f002904023f6b1dd8e7e9170c81d64
quincyocharles <quincyocharles@gmail.com>
2024-07-04 11:49:38 +0300

    Login and Logout functionality set up

--- mkt/store/views.py
+++ mkt/store/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . models import Product
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 
@@ -12,3 +14,30 @@ def home(request):
 def product(request, pk):
     product = (Product.objects.get(id=pk))
     return render(request, 'product.html', {'product': product})
+
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ("You have been logged in"))
+            return redirect('home')
+        else:
+            messages.success(request, ("There was an error, please try again"))
+            return redirect('login')
+
+    else:
+        return render(request, 'login.html', {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ("You have been logged out"))
+    return redirect('home')
+
+
+def register_user(request):
+    return render(request, 'register.html', {})
