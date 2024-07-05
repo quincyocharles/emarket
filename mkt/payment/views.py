d26fb80f3ebf30c2b3c2e392b65f395a9fb11461
quincyocharles <quincyocharles@gmail.com>
2024-07-05 12:28:38 +0300

    Implemented Order Generation when users click on the pay now button. The admin is able to see the items ordered in the backend

--- mkt/payment/views.py
+++ mkt/payment/views.py
@@ -65,3 +65,115 @@ def billing_info(request):
     else:
         messages.success(request, "Access Denied")
         return redirect('home')
+
+
+# process_order
+def process_order(request):
+    if request.POST:
+        # Get the cart
+        cart = Cart(request)
+        cart_products = cart.get_prods
+        quantities = cart.get_quants
+        totals = cart.cart_total()
+
+        # Get Billing Info from the last page
+        payment_form = PaymentForm(request.POST or None)
+        # Get Shipping Session Data
+        my_shipping = request.session.get('my_shipping')
+
+        # Gather Order Info
+        full_name = my_shipping['shipping_full_name']
+        email = my_shipping['shipping_email']
+        # Create Shipping Address from session info
+        shipping_address = f"{my_shipping['shipping_address1']}\n{my_shipping['shipping_address2']}\n{my_shipping['shipping_city']}\n{
+            my_shipping['shipping_state']}\n{my_shipping['shipping_zipcode']}\n{my_shipping['shipping_country']}"
+        amount_paid = totals
+
+        # Create an Order
+        if request.user.is_authenticated:
+            # logged in
+            user = request.user
+            # Create Order
+            create_order = Order(user=user, full_name=full_name, email=email,
+                                 shipping_address=shipping_address, amount_paid=amount_paid)
+            create_order.save()
+
+            # Add order items
+
+            # Get the order ID
+            order_id = create_order.pk
+
+            # Get product Info
+            for product in cart_products():
+                # Get product ID
+                product_id = product.id
+                # Get product price
+                if product.is_sale:
+                    price = product.sale_price
+                else:
+                    price = product.price
+
+                # Get quantity
+                for key, value in quantities().items():
+                    if int(key) == product.id:
+                        # Create order item
+                        create_order_item = OrderItem(
+                            order_id=order_id, product_id=product_id, user=user, quantity=value, price=price)
+                        create_order_item.save()
+
+            # Delete our cart
+            for key in list(request.session.keys()):
+                if key == "session_key":
+                    # Delete the key
+                    del request.session[key]
+
+            # Delete Cart from Database (old_cart field)
+            current_user = Profile.objects.filter(user__id=request.user.id)
+            # Delete shopping cart in database (old_cart field)
+            current_user.update(old_cart="")
+
+            messages.success(request, "Order Placed!")
+            return redirect('home')
+
+        else:
+            # not logged in
+            # Create Order
+            create_order = Order(full_name=full_name, email=email,
+                                 shipping_address=shipping_address, amount_paid=amount_paid)
+            create_order.save()
+
+            # Add order items
+
+            # Get the order ID
+            order_id = create_order.pk
+
+            # Get product Info
+            for product in cart_products():
+                # Get product ID
+                product_id = product.id
+                # Get product price
+                if product.is_sale:
+                    price = product.sale_price
+                else:
+                    price = product.price
+
+                # Get quantity
+                for key, value in quantities().items():
+                    if int(key) == product.id:
+                        # Create order item
+                        create_order_item = OrderItem(
+                            order_id=order_id, product_id=product_id, quantity=value, price=price)
+                        create_order_item.save()
+
+            # Delete our cart
+            for key in list(request.session.keys()):
+                if key == "session_key":
+                    # Delete the key
+                    del request.session[key]
+
+            messages.success(request, "Order Placed!")
+            return redirect('home')
+
+    else:
+        messages.success(request, "Access Denied")
+        return redirect('home')
