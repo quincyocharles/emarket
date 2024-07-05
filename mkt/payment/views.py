d68af4c6e481d7b0eb456cb566ea3a52e73ac946
quincyocharles <quincyocharles@gmail.com>
2024-07-05 12:01:06 +0300

    Created the billing info form that shows the summary of product ordered and customer shipping address, and also allows user to key in payment information

--- mkt/payment/views.py
+++ mkt/payment/views.py
@@ -34,3 +34,34 @@ def checkout(request):
         # Checkout as guest
         shipping_form = ShippingForm(request.POST or None)
         return render(request, "payment/checkout.html", {"cart_products": cart_products, "quantities": quantities, "totals": totals, "shipping_form": shipping_form})
+
+
+def billing_info(request):
+    if request.POST:
+        # Get the cart
+        cart = Cart(request)
+        cart_products = cart.get_prods
+        quantities = cart.get_quants
+        totals = cart.cart_total()
+
+        # Create a session with Shipping Info
+        my_shipping = request.POST
+        request.session['my_shipping'] = my_shipping
+
+        # Check to see if user is logged in
+        if request.user.is_authenticated:
+            # Get The Billing Form
+            billing_form = PaymentForm()
+            return render(request, "payment/billing_info.html", {"cart_products": cart_products, "quantities": quantities, "totals": totals, "shipping_info": request.POST, "billing_form": billing_form})
+
+        else:
+            # Not logged in
+            # Get The Billing Form
+            billing_form = PaymentForm()
+            return render(request, "payment/billing_info.html", {"cart_products": cart_products, "quantities": quantities, "totals": totals, "shipping_info": request.POST, "billing_form": billing_form})
+
+        shipping_form = request.POST
+        return render(request, "payment/billing_info.html", {"cart_products": cart_products, "quantities": quantities, "totals": totals, "shipping_form": shipping_form})
+    else:
+        messages.success(request, "Access Denied")
+        return redirect('home')
